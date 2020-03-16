2c45b5c6b910100885fd8bb6b0e802bd102e248b
imjoshellis <joshe523@gmail.com>
2020-03-15 20:50:34 -0500

    Update board game table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200316011551) do
+ActiveRecord::Schema.define(version: 20200316014913) do
 
   create_table "board_games", force: :cascade do |t|
     t.string "name"
