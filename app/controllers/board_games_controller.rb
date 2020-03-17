ba8e4f6d876da8b8269aeba5c3edfdeaa9af2e40
imjoshellis <joshe523@gmail.com>
2020-03-16 20:17:02 -0500

    Add logic to game management on /board-games index

--- app/controllers/board_games_controller.rb
+++ app/controllers/board_games_controller.rb
@@ -1,10 +1,12 @@
 class BoardGamesController < ApplicationController
 
   get "/board-games" do
+    @user = User.find(session[:user_id]) unless session[:user_id].nil?
     erb :'/board_games/index'
   end
 
   get "/boardgames" do
     redirect "/board-games"
+
   end
 end
