f246b038fadaf954ce5e7ee1e94dd6acab982d67
imjoshellis <joshe523@gmail.com>
2020-03-16 23:49:28 -0500

    Setup /board-games/:slug route

--- app/controllers/board_games_controller.rb
+++ app/controllers/board_games_controller.rb
@@ -8,7 +8,15 @@ class BoardGamesController < ApplicationController
     redirect "/board-games"
   end
 
+  get "/board-games/new" do
+  end
+
   post "/board-games/new" do
     # TODO: When you slug, make sure to remove colons and apostrophes, etc!
+  end
+
+  get "/board-games/:slug" do
+    @game = BoardGame.find_by(slug: params[:slug])
+    erb :show
   end
 end
