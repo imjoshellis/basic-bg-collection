0a1ac700a2b9749eba264234ff59ae9400d99111
imjoshellis <joshe523@gmail.com>
2020-03-16 22:19:24 -0500

    Create seed data for better CRUD testing

--- app/controllers/board_games_controller.rb
+++ app/controllers/board_games_controller.rb
@@ -1,5 +1,4 @@
 class BoardGamesController < ApplicationController
-
   get "/board-games" do
     @user = User.find(session[:user_id]) unless session[:user_id].nil?
     erb :'/board_games/index'
@@ -7,6 +6,9 @@ class BoardGamesController < ApplicationController
 
   get "/boardgames" do
     redirect "/board-games"
+  end
 
+  post "/board-games/new" do
+    # TODO: When you slug, make sure to remove colons and apostrophes, etc!
   end
 end
