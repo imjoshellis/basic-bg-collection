a2cfdf5c4f8670a70dbc61e1b606bef458217af4
Josh Ellis <joshe523@gmail.com>
2020-03-27 15:16:59 -0500

    Add custom validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ActiveRecord::Base
+  validates :username, :password, presence: true
+  validates :slug, uniqueness: true
+  validates :username, :password, length: {in: 3..12}
   has_secure_password
   has_many :user_games
   has_many :board_games, through: :user_games
